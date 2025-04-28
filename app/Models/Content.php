4c4a4b55a79ea42df09432c63aa22e024a2e0274
elnurbek <elnurbek.pubgmobile@gmail.com>
2025-04-28 14:48:52 +0500

    add fillable and relations

--- app/Models/Content.php
+++ app/Models/Content.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Content extends Model
 {
-    //
+    protected $fillable = ['title', 'description', 'url', 'category_id'];
+
+    protected $with = ['category'];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
