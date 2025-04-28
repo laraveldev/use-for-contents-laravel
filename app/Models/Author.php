44a7559a98dd861e5ada42d0d1d3d95d87fad7b7
elnurbek <elnurbek.pubgmobile@gmail.com>
2025-04-29 02:03:09 +0500

    add fillabe

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -1,10 +1,16 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
+    use HasFactory;
     protected $fillable=['name', 'url'];
+    
+    public function contents(): BelongsToMany
+    {
+        return $this->belongsToMany(Content::class);
+    }
 }
--- app/Models/Content.php
+++ app/Models/Content.php
@@ -1,16 +1,26 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Content extends Model
 {
+    use HasFactory;
     protected $fillable = ['title', 'description', 'url', 'category_id'];
 
     protected $with = ['category'];
 
     public function category(){
         return $this->belongsTo(Category::class);
+    }
+    public function authors()
+    {
+        return $this->belongsToMany(
+            Author::class,
+            'author_content', // pivot table name
+            'content_id', // foreign key on the pivot table for this model
+            'author_id', // foreign key on the pivot table for the related model
+        );
     }
 }
